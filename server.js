7fc61f5664a5d39b30e2a69cd93bf7c232da2f9e
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-25 20:59:36 +0530

    Write API using Express

--- server.js
+++ server.js
@@ -1,46 +1,23 @@
-// import http module to setup http server
-const http = require("http");
+// import express 
 
-// Create a server object
+const { response } = require('express');
+const express = require('express');
+const { get } = require('mongoose');
 
-const server = http.createServer((request, response) => {
-  // set the response header
-  /* response.writeHead(200, { 'Content-Type': 'json/application' });
-   */
-  // set the response body
-  /* response.end(JSON.stringify({ message: 'Hello world' })); */
-  
-  /* if (request.url === "/") {
-    if (request.method === "GET") {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.write(JSON.stringify({ message: "HEllo World!" }));
-      response.end();
-    } else if (request.method === "POST") {
-      response.writeHead(200, { "content-Type": "application/json" });
-      response.write(JSON.stringify({ message: "Hello world!" }));
-      response.end();
-    }
-  } */
+//create an express app 
 
-  // set the response header 
-  response.writeHead(200, { 'Content-Type': 'applicatiom/json' })
-  if (request.url === '/posts') {
-    //endpoints for posts 
-    if (request.method === 'GET') {
-      response.write(JSON.stringify ({ message : "GET POST"}))
-    } else if (request.method === 'POST') {
-      response.write(JSON.stringify({ message: 'POST POSTS' }));
-    }
-    
-  } else if (request.url === '/comments') {
-    response.write(JSON.stringify({ message: 'Comments' }));
-  }
-  response.end();
-  
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({ message: 'GEt /' })
 });
 
-//listento the server on port 3000
-
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Server is running on http://127.0.0.1:3000");
+app._router.post('/', (request, response) => {
+  response.json({ message: 'POST /' })
 });
+
+// listen for end point and start the server 
+app.listen(3000, () => {
+  console.log(`Server is running @ http://127.0.0.1:3000`)
+});
+
