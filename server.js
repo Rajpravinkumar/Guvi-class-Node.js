03b0580952e6d3d3fed5d23466a8f4f96e69d843
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-26 02:27:46 +0530

    connect database + dotenv

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 //import the app 
-
 const app = require('./app')
+// import mongoose 
+const mongoose = require('mongoose');
+//import dotenv
+require('dotenv').config();
 
 
-// listen for end point and start the server
-app.listen(3000, () => {
-  console.log(`Server is running @ http://127.0.0.1:3000`);
-});
+
+
+// connect to the database 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connecting to Database server running sucessfull...");
+
+    // listen for end point and start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running @ http://127.0.0.1:3000`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error Connecting to the DataBase", error);
+  });
