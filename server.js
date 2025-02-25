31149b3f2a61fcf09f673ed5b0a20d2e4a69e954
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-25 22:18:59 +0530

    middleware and customs midsileware

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
-// import express 
+// import express
 
-const { response } = require('express');
-const express = require('express');
-const { get } = require('mongoose');
+const { response } = require("express");
+const express = require("express");
+const { get } = require("mongoose");
 
-//create an express app 
+//create an express app
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'GEt /' })
+// use middleware 
+const logger = (request, response, next ) => {
+
+    console.log(`Request URL: ${request.url}`);
+  console.log(`Request Method: ${request.method}`);
+  console.log(`Request Headers: ${request.headers}`);
+  console.log(`Request Query: ${request.query}`);
+  console.log(`Request Params: ${request.params}`);
+  console.log(`Request Body: ${request.body}`);
+  console.log(`Request Cookies: ${request.cookies}`);
+  console.log(`-------------------`)
+
+  next();
+}
+app.use(logger);
+
+app.get("/", (request, response) => {
+response.json({ message: "GET /" });
 });
 
-app._router.post('/', (request, response) => {
-  response.json({ message: 'POST /' })
-});
+//middlewrae for handling 404 errors
 
-// listen for end point and start the server 
+const notFound = (request, response, next) => {
+  response.json({ message: 'Route not found' })
+  
+  next();
+}
+app.use(notFound);
+
+// listen for end point and start the server
 app.listen(3000, () => {
-  console.log(`Server is running @ http://127.0.0.1:3000`)
+  console.log(`Server is running @ http://127.0.0.1:3000`);
 });
-
