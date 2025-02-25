be10f598ce1e396a68275aa7fe76026b267cfdcf
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-25 23:04:32 +0530

    morgan --midleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const { get } = require("mongoose");
 const logger = require("./utils/logger");
 const errorRoutes = require("./utils/errorRoutes");
+//const morgan = require('morgan');
 
 //create an express app
 
@@ -13,8 +14,25 @@ const app = express();
 // use middleware
 app.use(logger);
 
+//use morgan third-part middleware
+//app.use(morgan('dev'));
+
 app.get("/", (request, response) => {
   response.json({ message: "GET /" });
+});
+
+app.post("/", (request, response) => {
+  response.json({ message: "post /" });
+});
+app.put("/", (request, response) => {
+  response.json({ message: "put /" });
+});
+app.delete("/", (request, response) => {
+  response.json({ message: "delete /" });
+});
+
+app.get("/post", (request, response) => {
+  response.json({ message: "GET POST /" });
 });
 
 //middlewrae for handling 404 errors
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //import the app 
 
-const app = require ('./app')
+const app = require('./app')
+
+
 // listen for end point and start the server
 app.listen(3000, () => {
   console.log(`Server is running @ http://127.0.0.1:3000`);
