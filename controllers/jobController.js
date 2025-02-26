e2af6ebe406166082c582b5f9987cae5635474b5
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-26 15:44:25 +0530

    models and define  schema + get data req + create new jon obj + save database + send responce

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,32 @@
-const { request, response } = require("../app");
+const Job = require('../models/Job')
 
 const jobController = {
-  getJobs: (request, response) => {
+  getJobs: async (request, response) => {
     response.json({ message: "GET" });
   },
 
-  createJobs: (request, response) => {
-    response.json({ message: "POST" });
+  
+
+  createJob: async (request, response) => {
+    // Get the data from the request body
+    const { title, company, location, salary, description, experience, skills } = request.body;
+
+    // Create a new job object
+    const newJob = new Job({
+      title,
+      company,
+      location,
+      salary,
+      description,
+      experience,
+      skills
+    });
+
+    // Save the job to the database
+    await newJob.save();
+
+    // Send a response
+    response.json({ message: 'Job created successfully' });
   },
 
   getJobID: (request, response) => {
@@ -14,7 +34,13 @@ const jobController = {
   },
   searchJobs: (request, response) => {
     response.json({ message: "GET BY SEARCH" });
-  }
-}
+  },
+  updateJobs: (request, response) => {
+    response.json({ message: "P" });
+  },
+  deleteJobs: (request, response) => {
+    response.json({ message: "GET BY SEARCH" });
+  },
+};
 
-    module.exports = jobController;
+module.exports = jobController;
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -5,12 +5,11 @@ const jobController = require("../controllers/jobController");
 // creater a express router
 const jobRouter = express.Router();
 
-
 jobRouter.get("/", jobController.getJobs);
-
-jobRouter.post("/", jobController.createJobs);
-
-jobRouter.get('/search', jobController.searchJobs);
+jobRouter.post("/", jobController.createJob);
 jobRouter.get("/:id", jobController.getJobID);
+jobRouter.get("/search", jobController.searchJobs);
+jobRouter.put("/:id", jobController.getJobID);
+jobRouter.delete("/:id", jobController.getJobID);
 
 module.exports = jobRouter;
